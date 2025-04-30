dd9e10306c1eee52062cb7be0df3f5eb81320d08
michaelyusak <michaelyusakt@gmail.com>
2025-04-30 16:29:23 +0700

    add postgres adaptor

---
+++ adaptor/postgres.go
@@ -0,0 +1,32 @@
+package adaptor
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/michaelyusak/go-helper/entity"
+	"github.com/sirupsen/logrus"
+)
+
+func ConnectPostgres(config entity.DBConfig, log *logrus.Logger) *sql.DB {
+	db, err := sql.Open("pgx", fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		config.Username, config.Password, config.Host, config.Port, config.DbName))
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("[adaptor][ConnectPostgres][Open] error: %s", err.Error()),
+		}).Fatal("error connecting to postgres")
+
+		return nil
+	}
+
+	if err = db.Ping(); err != nil {
+		log.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("[adaptor][ConnectPostgres][Ping] error: %s", err.Error()),
+		}).Fatal("error connecting to postgres")
+
+		return nil
+	}
+
+	return db
+}
---
+++ entity/db_config.go
@@ -0,0 +1,9 @@
+package entity
+
+type DBConfig struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	DbName   string `json:"db_name"`
+}
