10e9232745c1215acc64616c29e20e1027468c95
michaelyusak <michaelyusakt@gmail.com>
2025-04-30 16:38:48 +0700

    update postgres adaptor

--- adaptor/postgres.go
+++ adaptor/postgres.go
@@ -6,27 +6,18 @@ import (
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/michaelyusak/go-helper/entity"
-	"github.com/sirupsen/logrus"
 )
 
-func ConnectPostgres(config entity.DBConfig, log *logrus.Logger) *sql.DB {
+func ConnectPostgres(config entity.DBConfig) (*sql.DB, error) {
 	db, err := sql.Open("pgx", fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
 		config.Username, config.Password, config.Host, config.Port, config.DbName))
 	if err != nil {
-		log.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("[adaptor][ConnectPostgres][Open] error: %s", err.Error()),
-		}).Fatal("error connecting to postgres")
-
-		return nil
+		return nil, fmt.Errorf("[adaptor][ConnectPostgres][Open] error: %w", err)
 	}
 
 	if err = db.Ping(); err != nil {
-		log.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("[adaptor][ConnectPostgres][Ping] error: %s", err.Error()),
-		}).Fatal("error connecting to postgres")
-
-		return nil
+		return nil, fmt.Errorf("[adaptor][ConnectPostgres][Ping] error: %w", err)
 	}
 
-	return db
+	return db, nil
 }
