e9be9b27c637fcbff3dc9359c08e47d78f35485c
michaelyusak <michaelyusakt@gmail.com>
2025-09-13 19:51:46 +0700

    improve config loader: return error

--- config/load_json.go
+++ config/load_json.go
@@ -2,34 +2,21 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
-
-	"github.com/sirupsen/logrus"
 )
 
-func InitFromJson[T any](log *logrus.Logger, configFilePath string) T {
-	logHeading := "[go-helper][config][InitFromJson]"
-
+func InitFromJson[T any](configFilePath string) (T, error) {
 	var config T
 
 	configData, err := os.ReadFile(configFilePath)
 	if err != nil {
-		log.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("%s[os.ReadFile] error: %s", logHeading, err.Error()),
-		}).Fatal("error initiating config file")
-
-		return config
+		return config, err
 	}
 
 	err = json.Unmarshal(configData, &config)
 	if err != nil {
-		log.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("%s[json.Unmarshal] error: %s", logHeading, err.Error()),
-		}).Fatal("error initiating config file")
-
-		return config
+		return config, err
 	}
 
-	return config
+	return config, nil
 }
