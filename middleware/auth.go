55398466575f45adae9d16df7a4ba92403224ee5
michaelyusak <michaelyusakt@gmail.com>
2025-09-19 23:40:29 +0700

    add auth middleware

---
+++ middleware/auth.go
@@ -0,0 +1,57 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/michaelyusak/go-helper/appconstant"
+	"github.com/michaelyusak/go-helper/dto"
+	"github.com/michaelyusak/go-helper/helper"
+)
+
+type AuthOpt struct {
+	IsCheckDeviceId bool
+
+	Hash helper.HashHelper
+}
+
+type auth struct {
+	isCheckDeviceId bool
+
+	hash helper.HashHelper
+}
+
+func NewAuth(opt AuthOpt) *auth {
+	return &auth{
+		isCheckDeviceId: opt.IsCheckDeviceId,
+		hash:            opt.Hash,
+	}
+}
+
+func (m *auth) checkDeviceId(c *gin.Context, ipAddress, userAgent, referer string) {
+	if ipAddress == "" || userAgent == "" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, dto.ErrorResponse{Message: appconstant.MsgUnauthorized})
+		return
+	}
+
+	deviceId := m.hash.HashSHA512(fmt.Sprintf("%s:%s:%s", ipAddress, userAgent, referer))
+
+	c.Set(appconstant.DeviceIdKey, deviceId)
+}
+
+func (m *auth) Auth() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ipAddress := strings.TrimSpace(c.Request.Header.Get(appconstant.CfConnectingIp))
+		if ipAddress == "" {
+			ipAddress = c.ClientIP()
+		}
+
+		if m.isCheckDeviceId {
+			m.checkDeviceId(c, ipAddress, c.Request.UserAgent(), c.Request.Referer())
+		}
+
+		c.Next()
+	}
+}
