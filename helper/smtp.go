9915a5c1ec35d41f4dc9d5a77eeb1c8ee7dfecef
michaelyusak <michaelyusakt@gmail.com>
2025-10-14 22:06:24 +0700

    add smtp helper

---
+++ helper/smtp.go
@@ -0,0 +1,87 @@
+package helper
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/smtp"
+)
+
+type smtpHelper struct {
+	host     string
+	port     string
+	username string
+	password string
+	identity string
+}
+
+type SmtpHelperOpts struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	Identity string
+}
+
+func NewSmptpHelper(opt SmtpHelperOpts) *smtpHelper {
+	return &smtpHelper{
+		host:     opt.Host,
+		port:     opt.Port,
+		username: opt.Username,
+		password: opt.Password,
+		identity: opt.Identity,
+	}
+}
+
+type smtpRequest struct {
+	helper *smtpHelper
+
+	username   string
+	recipients []string
+	subject    string
+	body       string
+}
+
+func (h *smtpHelper) NewRequest(to []string, subject string) *smtpRequest {
+	return &smtpRequest{
+		helper: h,
+
+		username:   h.username,
+		recipients: to,
+		subject:    subject,
+	}
+}
+
+func (r *smtpRequest) SetBody(emailTemplate string, data any) error {
+	t, err := template.New("emailTemplate").Parse(emailTemplate)
+	if err != nil {
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, data)
+	if err != nil {
+		return err
+	}
+
+	r.body = buf.String()
+
+	return nil
+}
+
+func (r *smtpRequest) Send() error {
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	subject := "Subject: " + r.subject + "!\n"
+	msg := []byte(subject + mime + "\n" + r.body)
+
+	addr := fmt.Sprintf("%s:%s", r.helper.host, r.helper.port)
+
+	auth := smtp.PlainAuth(r.helper.identity, r.helper.username, r.helper.password, r.helper.host)
+
+	if err := smtp.SendMail(addr, auth, r.username, r.recipients, msg); err != nil {
+		return err
+	}
+
+	return nil
+}
